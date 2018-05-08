9d736d1906b5d968f13e78718540f87592224654
Garner Fox McCloud <garner@datadoghq.com>
2018-05-07 20:23:50 -0400

    Update dbclean.php

--- include/dbclean.php
+++ include/dbclean.php
@@ -26,4 +26,6 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 set_time_limit(360);
 $db->run("DELETE FROM worker_report WHERE date < UNIX_TIMESTAMP()-3600;");
 $db->run("OPTIMIZE TABLE worker_report");
+$db->run("DELETE FROM worker_discovery WHERE date < UNIX_TIMESTAMP()-3600;");
+$db->run("OPTIMIZE TABLE worker_discovery");
 ?>
